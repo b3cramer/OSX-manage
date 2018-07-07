6634c2b45c8b9287c5255b917bf516c806d386b6
Matt Cramer <b3cramer@gmail.com>
2018-07-07 14:46:40 -0600

    Homebrew/MAS update and cleanup script

---
+++ systemMaintenance.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#system updates using homebrew
+echo "Checking for updates..."
+brew update
+brew upgrade
+
+echo "Cleaning up..."
+brew cleanup -s
+brew cask cleanup
+
+#now diagnotic
+echo "Running diagnostics..."
+brew doctor
+brew missing
+
+#now check app store updates
+echo "Checking for outdated applications in the App Store..."
+mas outdated
+
+echo "Your system is all updated and cleaned up!"
