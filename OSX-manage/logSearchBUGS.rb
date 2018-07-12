c3fef72ea9b7c93b5fcbc2f4a288726d9817bccb
Matt Cramer <b3cramer@gmail.com>
2018-07-11 18:12:03 -0600

    Cleanup project directory.

--- OSX-manage/logSearchBUGS.rb
+++
@@ -1,5 +0,0 @@
-File.open("/private/var/log/system.log").each do |line|
-  if line.include?("BUG")
-    puts line
-  end
-end
--- OSX-manage/showADAccounts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-ADname=`dsconfigad -show | grep -i "Computer Account" | sed -n 's/[^.]*= // ; s/.$//p'`
-/bin/echo "<result>$ADname</result>"
-
-exit
