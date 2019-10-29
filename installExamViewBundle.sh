7798eecca499035ef2248f5cd1a4bb10639fc4d0
Matt Cramer <b3cramer@gmail.com>
2019-10-29 10:46:31 -0600

    Update AWS S3 download links

--- installExamViewBundle.sh
+++ installExamViewBundle.sh
@@ -2,15 +2,15 @@
 
 # download application packages from AWS S3 and install at "/"
 
-curl -O https://printdrivers.s3.amazonaws.com/ExamView_Mac.pkg
+curl -O https://secondarysoftwaredeploy.s3-us-west-2.amazonaws.com/ExamView_Mac.pkg
 echo "Exam View for Mac is installing now..."
   sudo installer -allowUntrusted -pkg ExamView_Mac.pkg -target /
 
-curl -O https://printdrivers.s3.amazonaws.com/ExamView+Installer.pkg
+curl -O https://secondarysoftwaredeploy.s3-us-west-2.amazonaws.com/ExamView+Installer.pkg
 echo "ExamView content is installing now..."
   sudo installer -allowUntrusted -pkg ExamView+Installer.pkg -target /
 
-curl -O https://printdrivers.s3.amazonaws.com/ExamView+Player+Installer.pkg
+curl -O https://secondarysoftwaredeploy.s3-us-west-2.amazonaws.com/ExamView+Player+Installer.pkg
 echo "ExamView Player is installing now..."
   sudo installer -allowUntrusted -pkg ExamView+Player+Installer.pkg -target /
 
